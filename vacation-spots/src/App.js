1f65eb69c53bca17f5d469a0219e151f8c9af4ff
Tony Tezak <>
2020-10-01 18:29:37 -1000

    initial commit

---
+++ vacation-spots/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+// import VacationData from "./VacationSpots"
+// import Vacay from "./Vacay"
+
+function App (){
+    return(
+        <h1>Aloha World! Ni Hao Shijie</h1>
+    )
+}
+
+export default App
