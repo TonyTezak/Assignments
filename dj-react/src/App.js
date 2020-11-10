538d5bf9bc41a0d91c245333b0d1aff6b0237dbb
Tony Tezak <anthonyjtezak@gmail.com>
2020-11-10 08:50:57 -1000

    My squares all disappeared...

--- dj-react/src/App.js
+++ dj-react/src/App.js
@@ -1,74 +1,72 @@
 import React from "react"
-import "./styles.css"
 import Square from "./Square"
+import "./styles.css"
+
 
 class App extends React.Component{
     constructor (){
         super ()
-        this.handleClickBW = this.handleClickBW.bind(this)
-        this.handleClickPurple = this.handleClickPurple.bind(this)
-        this.handleClickBlueLeft=this.handleClickBlueLeft.bind(this)
-        this.handleClickBlueRight=this.handleClickBlueRight.bind(this)
+        // this.handleClickBW = this.handleClickBW.bind(this)
+        // this.handleClickPurple = this.handleClickPurple.bind(this)
+        // this.handleClickBlueLeft=this.handleClickBlueLeft.bind(this)
+        // this.handleClickBlueRight=this.handleClickBlueRight.bind(this)
         this.state = {
-            squares:[
-            {position: 1, theme: "white"},
-            {position: 2, theme: "white"},
-            {position: 3, theme: "white"},
-            {position: 4, theme: "white"}]
+            squares: ['white', 'black', 'black', 'white']
         }
-        
-
     }
 
-    handleClickBW(){
-        this.setState(prevState => {
-            
-            return (
-            [{position: {gridColumn: 2/3, gridRow: 2/3}, theme: ".theme-black"}]
-            )
-        })
+    handleClickBW = () => {
+        this.setstate(({squares}) => ({squares: squares.map(squares => squares !== 'white' ? 'white' : 'black')}))
     }
+
+    //     this.setState(prevState => {    
+    //         return (
+    //         [{position: {gridColumn: 2/3, gridRow: 2/3}, theme: ".theme-black"}]
+    //         )
+    //     })
+    // }
     
+    // handleClickPurple = () => {
+    //     this.setState(prevState => {
+    //         return{
+    //             colors: prevState
+    //         }
+    //     })
+    // }
 
-    handleClickPurple(){
-        this.setState(prevState => {
-            return{
-                colors: prevState
-            }
-        })
-    }
+    // handleClickBlueLeft(){
+    //     this.setState(prevState => {
+    //         return{
+    //             colors: prevState
+    //         }
+    //     })
+    // }
 
-    handleClickBlueLeft(){
-        this.setState(prevState => {
-            return{
-                colors: prevState
-            }
-        })
-    }
-
-    handleClickBlueRight(){
-        this.setState(prevState => {
-            return{
-                colors: prevState
-            }
-        })
-    }
+    // handleClickBlueRight(){
+    //     this.setState(prevState => {
+    //         return{
+    //             colors: prevState
+    //         }
+    //     })
+    // }
 
     render(){
         
-        const colorSquares = this.state.squares.map(square => <Square key={square.position} color={square.color} position={square.position}/>)  
+        const colorSquares = this.state.squares.map(squares => <square color={squares}/>)
+            
+            // key={square.position} color={square.color} position={square.position}/>)  
 
         return (
-            <div>
+            <div className='squareContainer'>
                 {colorSquares}
-                <djSquare id={"square1"+ {gridColumn:2/3, gridRow:2/3}} className={"theme-" + {backgroundColor: ".theme-black"}}></djSquare>
-                <djSquare></djSquare>
-                <djSquare></djSquare>
-                <djSquare></djSquare>
+                <square id={"square1"+ {gridColumn:2/3, gridRow:2/3}} className={"theme-" + {backgroundColor: ".theme-black"}}></square>
+                <square></square>
+                <square></square>
+                <square></square>
                 <button onClick={this.handleClickBW}>Black/White</button>
-                <button onClick={this.handleClickPurple}>Purple</button>
+                {/* <button onClick={this.handleClickPurple}>Purple</button>
                 <button onClick={this.handleClickBlueLeft}>Blue Bottom L</button>
-                <button onClick={this.handleClickBlueRight}>Blue Bottom R</button>
+                <button onClick={this.handleClickBlueRight}>Blue Bottom R</button> */}
             </div>
         )
     }
--- dj-react/src/Square.js
+++ dj-react/src/Square.js
@@ -1,19 +1,26 @@
 import React from "react"
 
+const Square = (props) => {
+    return (
+        <div className='square' style={{backgroundColor: props.color}}></div>
+    )
+}
+
+export default Square
+
 // class Square extends React.Component{
 //     constructor(props)
 //     super(props);
 //     this.handleClickBW = this.handleClickBW.bind(this)
 // }
-function Square(props){
-const {position,color}=props
+// function Square(props){
+// const {position,color}=props
   
-return(
+// return(
 
-    <div>
-        <djSquare id={"square" + position} className={"theme-" + color}></djSquare>
-    </div>
-)
-}
+//     <div>
+//         <djSquare id={"square" + position} className={"theme-" + color}></djSquare>
+//     </div>
+// )
+// }
 
-export default Square
