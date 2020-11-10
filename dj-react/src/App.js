a0241c9dab44250c93848f5283f3c4c2340cc221
Tony Tezak <anthonyjtezak@gmail.com>
2020-11-10 13:11:26 -1000

    complete

--- dj-react/src/App.js
+++ dj-react/src/App.js
@@ -3,70 +3,88 @@ import Square from "./Square"
 import "./styles.css"
 
 
-class App extends React.Component{
+class App extends React.Component {
     constructor (){
         super ()
-        // this.handleClickBW = this.handleClickBW.bind(this)
-        // this.handleClickPurple = this.handleClickPurple.bind(this)
-        // this.handleClickBlueLeft=this.handleClickBlueLeft.bind(this)
-        // this.handleClickBlueRight=this.handleClickBlueRight.bind(this)
+
         this.state = {
             squares: ['white', 'black', 'black', 'white']
         }
     }
 
     handleClickBW = () => {
-        this.setstate(({squares}) => ({squares: squares.map(squares => squares !== 'white' ? 'white' : 'black')}))
+        //this.setState(({squares}) => ({squares: squares.map(square => square !== 'white' ? 'white' : 'black')}))
+       
+        this.setState(prevState => {
+            return {
+                squares: prevState.squares.map(square => {
+                    if(square !== 'white') {
+                        return 'white'
+                    }else {
+                        return 'black'
+                    }
+                })
+            }
+        })
+
+
     }
 
-    //     this.setState(prevState => {    
-    //         return (
-    //         [{position: {gridColumn: 2/3, gridRow: 2/3}, theme: ".theme-black"}]
-    //         )
-    //     })
-    // }
-    
-    // handleClickPurple = () => {
-    //     this.setState(prevState => {
-    //         return{
-    //             colors: prevState
-    //         }
-    //     })
-    // }
+    handleClickPurple = () => {
+        this.setState(prevState => {
+            return{
+                squares: prevState.squares.map((square, i) => {
+                    if (i <2){
+                        return 'purple'
+                    }else{
+                        return square
+                    }
+                })
+            }
+        })
+    }
 
-    // handleClickBlueLeft(){
-    //     this.setState(prevState => {
-    //         return{
-    //             colors: prevState
-    //         }
-    //     })
-    // }
+    handleClickBlueLeft = () => {
+        this.setState(prevState => {
+            return{
+                squares: prevState.squares.map((square, i) => {
+                    if (i === 2){
+                        return 'blue'
+                    }else{
+                        return square
+                    }
+                })
+            }
+        })
+    }
 
-    // handleClickBlueRight(){
-    //     this.setState(prevState => {
-    //         return{
-    //             colors: prevState
-    //         }
-    //     })
-    // }
+    handleClickBlueRight = () => {
+        this.setState(prevState => {
+            return{
+                squares: prevState.squares.map((square, i) => {
+                    if (i === 3){
+                        return 'blue'
+                    }else{
+                        return square
+                    }
+                })
+            }
+        })
+    }
 
     render(){
         
-        const colorSquares = this.state.squares.map(squares => <square color={squares}/>)
+        const colorSquares = this.state.squares.map(square => <Square color={square}/>)
             
-            // key={square.position} color={square.color} position={square.position}/>)  
-
         return (
-            <div className='squareContainer'>
-                {colorSquares}
-                <square id={"square1"+ {gridColumn:2/3, gridRow:2/3}} className={"theme-" + {backgroundColor: ".theme-black"}}></square>
-                <square></square>
-                <square></square>
-                <square></square>
+            <div>
+                <div className='squareContainer'>
+                    {colorSquares}
+                </div>
                 <button onClick={this.handleClickBW}>Black/White</button>
-                {/* <button onClick={this.handleClickPurple}>Purple</button>
+                <button onClick={this.handleClickPurple}>Purple</button>
                 <button onClick={this.handleClickBlueLeft}>Blue Bottom L</button>
-                <button onClick={this.handleClickBlueRight}>Blue Bottom R</button> */}
+                <button onClick={this.handleClickBlueRight}>Blue Bottom R</button>
             </div>
         )
     }
